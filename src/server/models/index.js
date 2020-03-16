5cb66178fb70c79985c005d678fa31bd76bf468f
Sharkanana <sharkanana@gmail.com>
2020-03-16 17:34:54 -0500

    Basic auth impl

---
+++ src/server/models/index.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+import UserSchema from './schemas/user';
+
+const connect = (uri) => {
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.set('useCreateIndex', true);
+  mongoose.Promise = global.Promise;
+
+  mongoose.connection.on('error', (err) => {
+    console.error(`Mongoose connection error: ${err}`);
+    process.exit(1);
+  });
+
+  mongoose.model('User', UserSchema);
+};
+
+export default connect;
